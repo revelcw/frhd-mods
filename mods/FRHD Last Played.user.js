636be90c0e808c3ec5e9e584895da442cd265cc3
revelcw <github@revelcw.com>
2021-07-17 18:32:32 -0400

    Update Last Played Mod

--- mods/FRHD Last Played.user.js
+++ mods/FRHD Last Played.user.js
@@ -8,7 +8,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
   let url;
   setInterval(() => {
@@ -22,37 +22,49 @@
   }, 500);
 
   const getUserData = async (user) => {
-    const resp = await fetch(`https://www.freeriderhd.com/u/${user}?ajax=true`)
+    const resp = await fetch(`https://www.freeriderhd.com/u/${user}?ajax=true`);
     if (resp.ok) {
-      return await resp.json()
+      return await resp.json();
     }
-  }
+  };
 
   const getUserActivity = async (user) => {
-    const userData = await getUserData(user)
+    const userData = await getUserData(user);
     if (userData?.friends.friends_data.length > 0) {
-      const friend = userData.friends.friends_data[0]
-      const friendData = await getUserData(friend.u_name)
-      return friendData.friends.friends_data.find((friend) => friend.u_name.toLowerCase() === user.toLowerCase()).activity_time_ago
+      const friend = userData.friends.friends_data[0];
+      const friendData = await getUserData(friend.u_name);
+      return friendData.friends.friends_data.find(
+        (friend) => friend.u_name.toLowerCase() === user.toLowerCase()
+      ).activity_time_ago;
     } else {
-      throw new Error("User not defined / User has no friends")
+      throw new Error('User not defined / User has no friends');
     }
-  }
+  };
 
   const main = async () => {
     let timer = setInterval(async () => {
-      if (document.querySelector("#content > div > div.profile-header > div.profile-user-info.clearfix > div > div.profile-info > div:nth-child(2)")) {
-        clearInterval(timer)
-        const url = new URL(document.URL)
-        const user = url.pathname.split("/")[2]
-        const lastPlayed = await getUserActivity(user)
+      if (
+        document.querySelector(
+          '#content > div > div.profile-header > div.profile-user-info.clearfix > div > div.profile-info > div:nth-child(2)'
+        )
+      ) {
+        clearInterval(timer);
+        const url = new URL(document.URL);
+        const user = url.pathname.split('/')[2];
+        const lastPlayed = await getUserActivity(user);
         if (lastPlayed) {
-          const parent = document.querySelector("#content > div > div.profile-header > div.profile-user-info.clearfix > div > div.profile-info > div:nth-child(2)")
-          const text = document.createElement("span")
-          text.classList.add("profile-blurb");
-          text.style = 'position: absolute;padding: 2px;padding-left: 5px;'
-          text.textContent = "Last Played " + lastPlayed
-          parent.appendChild(text);
+          const parent = document.querySelector(
+            '#content > div > div.profile-header > div.profile-user-info.clearfix > div > div.profile-info'
+          );
+          const ref = document.querySelector(
+            '#content > div > div.profile-header > div.profile-user-info.clearfix > div > div.profile-info > div:nth-child(3)'
+          );
+          const text = document.createElement('div');
+          text.classList.add('profile-blurb');
+          text.classList.add('flex-item');
+          text.style = 'padding: 2px 3px 3px 0px;font-size: 12.5px;';
+          text.textContent = 'Last Played ' + lastPlayed;
+          parent.insertBefore(text, ref);
         }
       }
     });
